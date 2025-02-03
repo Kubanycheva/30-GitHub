e36982eda6dddbf0edbf03eb6d68b4108987c65c
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2025-02-03 22:22:00 +0600

    multiselectfield

--- course/models.py
+++ course/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
+from django.core.validators import MinValueValidator, MaxValueValidator
+from multiselectfield import MultiSelectField
+
+
 
 class UserProfile(models.Model):
