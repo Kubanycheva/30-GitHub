23885be01d91116f511e60a72a3fc28865d75fbd
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2025-02-03 22:20:38 +0600

    phone_number

--- mysite/settings.py
+++ mysite/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'course',
-    'rest_framework'
+    'rest_framework',
+    "phonenumber_field",
 ]
 
 MIDDLEWARE = [
