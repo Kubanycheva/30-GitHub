5d17bdc055d007ed217a0da7f02729212a84eb58
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2025-02-04 22:47:13 +0600

    again admin and serializers

--- course/admin.py
+++ course/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(UserProfile)
-admin.site.register(UserProfile)
+admin.site.register(Network)
 admin.site.register(UserProfile)
 admin.site.register(UserProfile)
 admin.site.register(UserProfile)
--- course/serializers.py
+++ course/serializers.py
@@ -1,1 +1,7 @@
 from rest_framework import serializers
+from .models import *
+
+class UserProfile(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
