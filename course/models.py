536b3b8e91cbea8ef908695dc98da02565ae29e6
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2025-02-05 01:29:52 +0600

    CASCADE

--- course/models.py
+++ course/models.py
@@ -226,6 +226,13 @@ class FavoriteItem(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
 
+class Country(models.Model):
+    country_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.country_name
+
+
 class Order(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='client')
     cart_item = models.ForeignKey(CartItem, on_delete=models.CASCADE)
@@ -239,6 +246,7 @@ class Order(models.Model):
     expiration_date = models.DateField()
     cvv = models.DecimalField(max_digits=3, decimal_places=0)
     created_date = models.DateTimeField(auto_now_add=True)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.student}, {self.status}'
