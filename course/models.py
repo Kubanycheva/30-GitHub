0dc501d3f9d975b2d031fc800a8e132fff530ca5
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2025-02-04 12:19:16 +0600

    course

--- course/models.py
+++ course/models.py
@@ -14,6 +14,8 @@ STATUS_CHOICES = (
     ('средний', 'средний'),
     ('сложный', 'сложный')
 )
+
+
 class UserProfile(AbstractUser):
     phone_number = PhoneNumberField(null=True, blank=True)
     age = models.PositiveSmallIntegerField(validators=[MinValueValidator(15),
@@ -25,3 +27,9 @@ class UserProfile(AbstractUser):
         return f'{self.first_name}, {self.last_name}'
 
 
+class Network(models.Model):
+    network_name = models.CharField(max_length=32, null=True, blank=True)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user}, {self.network_name}'
