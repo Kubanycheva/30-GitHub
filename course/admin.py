aaa0b2a8e5858c0073987ea988fda00d1d976cac
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2025-02-04 22:44:51 +0600

    all commit

--- course/admin.py
+++ course/admin.py
@@ -2,3 +2,15 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(UserProfile)
+admin.site.register(UserProfile)
+admin.site.register(UserProfile)
+admin.site.register(UserProfile)
+admin.site.register(UserProfile)
+admin.site.register(UserProfile)
+admin.site.register(UserProfile)
+admin.site.register(UserProfile)
+admin.site.register(UserProfile)
+admin.site.register(UserProfile)
+admin.site.register(UserProfile)
+admin.site.register(UserProfile)
+admin.site.register(UserProfile)
