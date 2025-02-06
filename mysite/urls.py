1d06883861237212fec877809bb053d8344efca7
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2025-02-07 00:05:48 +0600

    3.12

--- mysite/urls.py
+++ mysite/urls.py
@@ -35,7 +35,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path()
+    path('', include())
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
