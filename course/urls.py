a762512de21f75c85b6d732c8c7d1049dc045f4e
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2025-02-07 21:14:29 +0600

    router and urls

--- course/urls.py
+++ course/urls.py
@@ -3,8 +3,12 @@ from .views import *
 from rest_framework import routers
 
 router = routers.SimpleRouter()
-router.register(r'user')
+router.register(r'user', UserProfile, basename='users')
+router.register(r'network', NetworkViewSet, basename='networks')
+
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+
+    path('course/', CourseListApiView)
 
 ]
