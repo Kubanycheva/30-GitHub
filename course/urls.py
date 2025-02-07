69e358052450e7f083e18b3dc0fec68dd707814d
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2025-02-07 19:33:08 +0600

    router

--- course/urls.py
+++ course/urls.py
@@ -2,3 +2,9 @@ from django.urls import path,  include
 from .views import *
 from rest_framework import routers
 
+router = routers.SimpleRouter()
+router.register(r'user')
+urlpatterns = [
+    path('', include(router.urls))
+
+]
