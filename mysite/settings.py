16933fcb0587e8885b8cf8a3e9681bd7bbe30f60
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2025-02-17 23:26:43 +0600

    opers

--- mysite/settings.py
+++ mysite/settings.py
@@ -108,10 +108,4 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
 
