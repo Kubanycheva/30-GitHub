346c3de2e5a087ca06503f70bef5b78228dc619d
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2025-02-17 23:24:44 +0600

    settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,9 +133,3 @@ LANGUAGES = (
     ('ru', 'Russian'),
     ('en', 'English'),
 )
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
