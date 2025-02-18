9366856d12100f253ba93d38dd12c999237c515c
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2025-02-18 23:29:33 +0600

    again settings and than will be writing

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,12 +37,4 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'modeltranslation',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'course',
-    'rest_framework',
-    "phonenumber_field",
+    'django.contri
