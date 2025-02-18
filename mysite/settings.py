e93ab9cffaba2aaa9c266b0f4c305fe0632eddf0
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2025-02-18 23:20:24 +0600

    again

--- mysite/settings.py
+++ mysite/settings.py
@@ -146,6 +146,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'd
