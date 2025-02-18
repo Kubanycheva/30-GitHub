cd3376413400fcb2e462f9fc24266cdbd8ef2714
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2025-02-18 23:26:29 +0600

    asdfgh

--- mysite/settings.py
+++ mysite/settings.py
@@ -90,12 +90,3 @@ MODELTRANSLATION_LANGUAGES = ('en', 'ru')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-# Password validation
-# https://docs.djangoproject.c
