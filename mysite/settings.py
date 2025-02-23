0eed82095e96d6867a6afab35c7274de32a26bf7
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2025-02-23 22:39:04 +0600

    course

--- mysite/settings.py
+++ mysite/settings.py
@@ -148,7 +148,4 @@ MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'course.UserProfile'
 
