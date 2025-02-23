f1b6f789d06258d666bb0baa2e7436ecd4bd6bab
Kybanychevalililili <118038085+Kybanychevalililili@users.noreply.github.com>
2025-02-23 22:40:43 +0600

    course

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,14 +75,3 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'mysite.wsgi.application'
-
-MODELTRANSLATION_DEFAULT_LANGUAGE = 'en'
-
-MOD
